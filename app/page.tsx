d2fef367dd7f232cfc375f92852b5d57b2a3852b
Texeraaa <pedro.henrique.ph2018@hotmail.com>
2025-08-05 03:15:39 -0300

    feat: restructure Home component to include About and Hero sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import About from "@/components/About";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     
+    <div className="min-h-screen">
+     <main>
+       <section id="home">
+          <Hero />
+        </section>
+        <section id="about">
+          <About />
+        </section>
+     </main>
     </div>
   );
 }
